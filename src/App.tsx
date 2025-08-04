86733016c249ef932bb919f9d4c392a414d4db78
Guilherme Campos <guilhermev272@gmail.com>
2025-08-03 21:35:02 -0300

    Create Files: Início do projeto.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  const message: string = "Hello World!";
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-black text-3xl font-bold ">{message}</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
